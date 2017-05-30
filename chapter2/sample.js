8d042f91f729374f06cca15240ec8f9144a15c7f
yinm <yinm.cminor9@gmail.com>
2017-05-31 08:16:35 +0900

    2-5-4

--- chapter2/sample.js
+++ chapter2/sample.js
@@ -1,12 +1,8 @@
 let obj = { x:3, y:4 };
-console.log(typeof obj);
-console.log(obj.x);
+console.log(obj['x']);
 
-let obj2 = { pos: { x:3, y:4 } };
-console.log(obj2.pos.x);
+let name = 'x';
+console.log(obj[name]);
 
-obj.x = 33;
-console.log(obj.x);
-
-obj.z = 5;
+obj['z'] = 5;
 console.log(obj.z);
