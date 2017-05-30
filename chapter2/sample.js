66639eeb74560018a5cfbd2f94857aed19f87b29
yinm <yinm.cminor9@gmail.com>
2017-05-31 08:01:28 +0900

    2-5-3

--- chapter2/sample.js
+++ chapter2/sample.js
@@ -1,2 +1,12 @@
-let obj = {x:3, y:4};
+let obj = { x:3, y:4 };
 console.log(typeof obj);
+console.log(obj.x);
+
+let obj2 = { pos: { x:3, y:4 } };
+console.log(obj2.pos.x);
+
+obj.x = 33;
+console.log(obj.x);
+
+obj.z = 5;
+console.log(obj.z);
